a233c09336044cba0f28214551f10922425338c4
Aykut İsmet Aslantaş <aykur111aykur@gmail.com>
2020-03-21 22:06:00 +0300

    Add files via upload

---
+++ Girilen Sayının Tek mi Çift mi Olduğunu Bulan C# Uyguluması.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sayının_Tek_mi_Çift_mi_Olduğunu_Bulan_C_Uygulumas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi;
+            Console.Write("Sayı Giriniz");
+            sayi = Convert.ToInt32(Console.ReadLine());
+            if ((sayi % 2) == 0)
+            {
+                Console.Write("Sayı Çifttir");
+            }
+            else Console.Write("Sayı Tektir");
+            Console.ReadKey();
+            }
+        
+                
+        }
+
+    }
+
